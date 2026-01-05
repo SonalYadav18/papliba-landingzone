0cd4518255ef337e60a2b589a5eceff18f0bdef6
Sonal Yadav <sonalyadav181991@gmail.com>
2026-01-05 19:47:33 +0200

    created my first management group using tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Specifiy the provider and version
+terraform {
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 4.0"
+        }
+    }
+
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    features {}
+    subscription_id = "26aa5846-d34c-410e-a041-1bfe17406a4e"
+}   
+
+# Create the very first management group
+resource "azurerm_management_group" "papliba_mg" {
+    display_name = "papliba_mg"
+  
+}
